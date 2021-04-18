ef65e3bd08d1529bf4de0fc0381cadf15487d279
Gabriel Ramos <gabrieljhramos@gmail.com>
2021-04-18 13:35:33 -0300

    Push desde eclipse

--- TP 1/src/ejercicio1/HolaMundo.java
+++ TP 1/src/ejercicio1/HolaMundo.java
@@ -1,9 +1,10 @@
 package ejercicio1;
 
-public class holaMundo {
+public class HolaMundo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Hola Mundo");
 
 	}
 
