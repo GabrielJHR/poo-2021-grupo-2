afe209eee6a1879cdd24416660456921d32ed3f7
CurtiStyle <seb_cep_12@hotmail.com>
2021-04-23 17:07:03 -0300

    4/23/21 17:06

--- TP2/src/ejercicio3/Palindromo.java
+++ TP2/src/ejercicio3/Palindromo.java
@@ -10,11 +10,36 @@ public class Palindromo {
 		
 		Scanner teclado = new Scanner(System.in);
 
-		String cadena, aux;
+		String cadena;
+		String cadenaInvertida;
 		
-	//	StringBuilder invertir = new StringBuilder(System.in)
-				
+		System.out.println("Ingrese una cadena: ");
+		
+		cadena = teclado.next();		
+		cadenaInvertida = invertir(cadena);
+		
+		System.out.println("Cadena: " + cadena);		
+		System.out.println("Cadena Invertida: " + cadenaInvertida);	
+		
+		if (cadena.equals(cadenaInvertida)) {
+			System.out.println("La cadena es Palindromo.");
+		}else {
+			System.out.println("La cadena no es Palindromo.");
+		}
+		
 		
 	}
 
+	
+	public static String invertir(String cad) {
+		String cadInvertida;
+		cadInvertida = "";
+		for (int i = cad.length()-1; i >= 0; i--) {
+			cadInvertida = cadInvertida + cad.charAt(i);
+		}
+		return cadInvertida;
+		
+		
+	}
+	
 }
