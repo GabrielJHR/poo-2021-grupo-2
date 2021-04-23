c4a0827cf9771808869e5621bcb4a165259814ff
CurtiStyle <seb_cep_12@hotmail.com>
2021-04-22 21:29:15 -0300

    nuevo 4/22/21

---
+++ TP2/src/ejercicio3/Palindromo.java
@@ -0,0 +1,20 @@
+package ejercicio3;
+
+import java.util.Scanner;
+import java.lang.StringBuilder;
+
+
+public class Palindromo {
+
+	public static void main(String[] args) {
+		
+		Scanner teclado = new Scanner(System.in);
+
+		String cadena, aux;
+		
+		StringBuilder invertir = new StringBuilder(System.in)
+				
+		
+	}
+
+}
