81a6cc825337cc875247a3a60c0e81b02e7302c6
CurtiStyle <seb_cep_12@hotmail.com>
2021-04-23 18:15:59 -0300

    23/04/21 18:15

--- TP2/src/ejercicio4/PruebaArrays.java
+++ TP2/src/ejercicio4/PruebaArrays.java
@@ -1,10 +1,86 @@
 package ejercicio4;
 
+import java.util.Arrays;
+
 public class PruebaArrays {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] numeros = {4,2,3,8,1};
+	
+		mostrar(numeros);
+		
+		
 	}
+	
+	public static void mostrar(int[] vector) {
+		
+		System.out.println("Arreglo: " + Arrays.toString(vector));
+		
+		System.out.println("Ordenar de Menor a mayor"  + Arrays.toString(menorMayor(vector)));
+		
+		System.out.println("Ordenar de Mayor a Menor"  + Arrays.toString(mayorMenor(vector)));
+		
+		System.out.println("Promedio: "  + promedio(vector));
+		
+		System.out.println("Tamaño del arreglo: " + vector.length);
+		
+		System.out.println("Mayor: " + mayor(vector));
+		
+	}
+	
 
+	public static int[] menorMayor(int[] vector) {
+		int aux;
+		
+		for(int i = 0; i <= 3; i++ ) {
+			
+			for(int j = i+1; j <= 4; j++ ) {
+				
+				if (vector[i] > vector[j]) {
+					aux = vector[i];
+					vector[i] = vector[j];
+					vector[j] = aux;
+				}
+			}
+		}
+	return vector;	
+	}
+	
+	public static int[] mayorMenor(int[] vector) {
+		int aux;
+		
+		for(int i = 0; i <= vector.length; i++ ) {
+			
+			for(int j = i+1; j <= vector.length-1; j++ ) {
+				
+				if (vector[i] < vector[j]) {
+					aux = vector[i];
+					vector[i] = vector[j];
+					vector[j] = aux;
+				}
+			}
+		}
+	return vector;	
+	}
+	
+	public static double promedio(int[] vector) {
+		int acomulador = 0;
+		for (int i = 0; i < vector.length ; i++) {
+			acomulador += vector[i];
+			
+		}
+		return acomulador/vector.length;
+	}
+	
+	
+	public static int mayor(int[] vector) {
+		int aux = 0;
+		for (int i=0; i < vector.length ; i++) {
+			if (aux < vector[i]) {
+				aux = vector[i];
+			}
+		}
+		return aux;
+	}
+	
 }
